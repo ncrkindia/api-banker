43c66fdb4730febdb051687ad4ebea5a5ff5e73c
NCRK India Admin <35381426+ncrkindia@users.noreply.github.com>
2026-08-07 11:45:42 +0530

    docs: add commented-out Java 21 environment configuration to startup scripts

--- apibanker.bat
+++ apibanker.bat
@@ -1,4 +1,7 @@
 @echo off
+@REM REM Set Java21 or above for running the application
+@REM REM set JAVA_HOME=C:\Program Files\Java\jdk-21
+@REM REM set PATH=%JAVA_HOME%\bin;%PATH%
 start javaw -jar apibanker-${project.version}.jar
 exit
 
--- apibanker.sh
+++ apibanker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Set Java21 or above for running the application
+# export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
+# export PATH=$JAVA_HOME/bin:$PATH
 java -jar apibanker-${project.version}.jar &
 
